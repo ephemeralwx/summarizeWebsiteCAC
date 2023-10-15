e7d0e7220bb6e7b325d6313fd80f7501f6a15f10
Kevin <kevinx8017@gmail.com>
2023-10-15 14:28:29 -0400

    chnaged some stuff

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 OPENAI_API_ENDPOINT = "https://api.openai.com/v1/engines/davinci/completions"
 
+# Set a limit for the number of characters to pass to the model
+MAX_CHARS_FOR_SUMMARY = 2000
 
 @app.route('/fetch_text', methods=['GET'])
 def fetch_body_text_api():
@@ -48,12 +50,15 @@ def generate_summary_api():
         paragraphs = [p.get_text().strip() for p in soup.find_all('p')]
         body_text = " ".join(paragraphs)
         
+        # Limit the text to a certain number of characters for summarization
+        limited_text = body_text[:MAX_CHARS_FOR_SUMMARY]
+
         headers = {
             "Authorization": f"Bearer {OPENAI_API_KEY}",
             "Content-Type": "application/json"
         }
         data = {
-            "prompt": f"Given this text: {body_text}, generate a summary",
+            "prompt": f"Summarize the following content in a concise manner: {limited_text}",
             "max_tokens": 500
         }
         openai_response = requests.post(OPENAI_API_ENDPOINT, headers=headers, json=data)
